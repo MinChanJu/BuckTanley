fa326644408c488c157de99a99d1bab877211a56
MinChanJu <mcj00220077@gmail.com>
2025-01-22 11:00:09 +0900

    오타 수정

--- buck_tanley/src/main/java/com/example/buck_tanley/controller/UserController.java
+++ buck_tanley/src/main/java/com/example/buck_tanley/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.example.buck_tanley.contoller;
+package com.example.buck_tanley.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
