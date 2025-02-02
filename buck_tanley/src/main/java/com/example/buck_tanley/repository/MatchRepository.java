df3966b91392d8f1dde1df16057454ec72641b8f
MinChanJu <mcj00220077@gmail.com>
2025-02-02 16:50:58 +0900

    match, message 서비스 CRUD 기능 구현

--- buck_tanley/src/main/java/com/example/buck_tanley/repository/MatchRepository.java
+++ buck_tanley/src/main/java/com/example/buck_tanley/repository/MatchRepository.java
@@ -1,5 +1,7 @@
 package com.example.buck_tanley.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import com.example.buck_tanley.domain.entity.Match;
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
     boolean existsByUserId1AndUserId2(String userId1, String userId2);
+
+    List<Match> findAllByUserId1(String userId1);
+
+    List<Match> findAllByUserId2(String userId2);
 }
--- buck_tanley/src/main/java/com/example/buck_tanley/service/MatchService.java
+++ buck_tanley/src/main/java/com/example/buck_tanley/service/MatchService.java
@@ -1,5 +1,8 @@
 package com.example.buck_tanley.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +17,31 @@ public class MatchService {
     @Autowired
     private MatchRepository matchRepository;
 
-    public Match createMatch(Match match) {
+    public List<Match> GetAllMatchByUserId(String userId) {
+        List<Match> matchs1 = matchRepository.findAllByUserId1(userId);
+        List<Match> matchs2 = matchRepository.findAllByUserId2(userId);
+
+        List<Match> combinedMatches = new ArrayList<>(matchs1);
+        combinedMatches.addAll(matchs2);
+        return combinedMatches;
+    }
+
+    public Match CreateMatch(Match match) {
         if (matchRepository.existsByUserId1AndUserId2(match.getUserId1(), match.getUserId2()) ||
                 matchRepository.existsByUserId1AndUserId2(match.getUserId2(), match.getUserId1())) {
             throw new CustomException(ErrorCode.USER_NOT_FOUND);
         }
         return matchRepository.save(match);
+    }
+
+    public Match UpdateMatch(Match match) {
+        if (matchRepository.existsById(match.getId())) throw new CustomException(ErrorCode.MATCH_NOT_FOUND);
+        return matchRepository.save(match);
+    }
+
+    public void DeleteMatch(Long id) {
+        if (id == null) throw new CustomException(ErrorCode.MATCH_NOT_FOUND);
+        if (matchRepository.existsById(id)) throw new CustomException(ErrorCode.MATCH_NOT_FOUND);
+        matchRepository.deleteById(id);
     }
 }
