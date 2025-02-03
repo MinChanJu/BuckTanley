1b9b2508615b79d01b111536c60bbfebeb0f190e
wodud1107 <wodud7992@naver.com>
2025-02-03 16:27:28 +0900

    service 생성 조회 수정 삭제 구현

--- buck_tanley/src/main/java/com/example/buck_tanley/service/UserService.java
+++ buck_tanley/src/main/java/com/example/buck_tanley/service/UserService.java
@@ -2,11 +2,14 @@ package com.example.buck_tanley.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.buck_tanley.domain.entity.User;
 import com.example.buck_tanley.exception.CustomException;
 import com.example.buck_tanley.exception.ErrorCode;
 import com.example.buck_tanley.repository.UserRepository;
+
+import java.util.*;
 
 @Service
 public class UserService {
@@ -14,8 +17,63 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User creatUser(User user) {
-        if (userRepository.existsByUserId(user.getUserId())) throw new CustomException(ErrorCode.DUPLICATE_USER_ID);
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public User getUserById(Long id) {
+        if (id == null)
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty())
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+
+        return user.get();
+    }
+
+    @Transactional
+    public User createUser(User user) {
+        if (userRepository.existsByUserId(user.getUserId()))
+            throw new CustomException(ErrorCode.DUPLICATE_USER_ID);
+
+        String password = user.getUserPw();
+        if (!password.matches(".*[a-zA-Z].*"))
+            throw new CustomException(ErrorCode.INVALID_PASSWORD_ALP); // 영문자 포함 확인
+
+        if (!password.matches(".*\\d.*"))
+            throw new CustomException(ErrorCode.INVALID_PASSWORD_NUM); // 숫자 포함 확인
+
+        if (!password.matches("[a-zA-Z0-9]+"))
+            throw new CustomException(ErrorCode.INVALID_PASSWORD_SPE); // 특수문자 포함 확인
+
+        if (password.length() < 10)
+            throw new CustomException(ErrorCode.INVALID_PASSWORD_LEN); // 길이 10자 이상 확인
+
         return userRepository.save(user);
+    }
+
+    @Transactional
+    public User updateUser(User user) {
+        if (user.getId() == null)
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+
+        Optional<User> findUser = userRepository.findById(user.getId());
+        if (findUser.isEmpty())
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteUser(Long id) {
+        if (id == null)
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty())
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+
+        userRepository.delete(user.get());
     }
 }
