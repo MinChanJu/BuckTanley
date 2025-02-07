3306dcb63f1c005b3f2b124b5b9e6ad23357e10b
wodud1107 <wodud7992@naver.com>
2025-02-07 16:17:45 +0900

    LoginPage 구현

---
+++ buck_tanley_app/lib/pages/LoginPage.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: SizedBox(
+          height: 50,
+          child: Image.asset('assets/images/BuckTanleyLogo.png',
+              fit: BoxFit.contain),
+        ),
+      ),
+      body: Column(
+        children: [
+          const Spacer(),
+
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 5,
+                      spreadRadius: 2,
+                      offset: Offset(2, 2),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(labelText: 'ID'),
+                    ),
+                    const SizedBox(height: 10),
+                    const TextField(
+                      decoration: InputDecoration(labelText: 'PassWord'),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('로그인')
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text('ID 찾기  |  PW 찾기  |  회원가입'),
+          ),
+
+          const Spacer(),
+
+          // 광고 영역  - 배너 클릭시 링크 연결되게 수정해야 하니 버튼으로 변경해야함
+          Container(
+            color: Colors.grey[300],
+            height: 100,
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: const Text('광고'),
+          ),
+        ],
+      ),
+    );
+  }
+}
