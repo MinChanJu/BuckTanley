b66a10447c4219b145e10832bf4558f9b63079a9
wodud1107 <wodud7992@naver.com>
2025-02-18 17:49:43 +0900

    유저 컨트롤러, 매치 컨트롤러 구현

--- buck_tanley/src/main/java/com/example/buck_tanley/repository/UserRepository.java
+++ buck_tanley/src/main/java/com/example/buck_tanley/repository/UserRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.example.buck_tanley.domain.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUserId(String userId);
+    Optional<User> findByUserId(String userId);
 }
