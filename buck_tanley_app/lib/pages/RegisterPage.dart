fd638465240f32536d2fc7fec818fbb51ac6ab1c
wodud1107 <wodud7992@naver.com>
2025-02-20 16:25:05 +0900

    register api 구현, 서비스 수정

--- buck_tanley_app/lib/pages/RegisterPage.dart
+++ buck_tanley_app/lib/pages/RegisterPage.dart
@@ -1,8 +1,13 @@
-import 'dart:io';
+import 'dart:convert';
+import 'dart:io' as io;
+import 'dart:typed_data';
 
+import 'package:buck_tanley_app/utils/Server.dart';
 import 'package:buck_tanley_app/widgets/AdBanner.dart';
 import 'package:buck_tanley_app/widgets/LogoAppBar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -15,194 +20,230 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _idController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _nicknameController = TextEditingController(); // 닉네임
+  final TextEditingController _nicknameController =
+      TextEditingController(); // 닉네임
   final TextEditingController _bioController = TextEditingController(); // 자기소개
   final TextEditingController _ageController = TextEditingController(); // 생년월일
   final TextEditingController _phoneController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController(); // 이메일
-  String? selectedGender;
-  File? _image;
+  final TextEditingController _emailController = TextEditingController();
+  bool? selectedGender;
+  Uint8List? _webImage; // 웹 환경 이미지
+  io.File? _image; // 모바일 환경 이미지
 
   Future<void> _pickImage() async {
     final pickedFile =
         await ImagePicker().pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
+      if (kIsWeb) {
+        final bytes = await pickedFile.readAsBytes();
+        setState(() {
+          _webImage = bytes;
+        });
+      } else {
+        setState(() {
+          _image = io.File(pickedFile.path);
+        });
+      }
     }
+  }
+
+  Future<void> _registerUser() async {
+    final url = Uri.parse('${Server.url}/users/register');
+    final headers = {
+      'Accept': 'application/json; charset=UTF-8',
+      'Content-Type': 'application/json; charset=UTF-8',
+    };
+    final body = jsonEncode({
+      "userId": _idController.text,
+      "userPw": _passwordController.text,
+      "name": _nicknameController.text,
+      "introduction": _bioController.text,
+      "age": int.parse(_ageController.text),
+      "phone": _phoneController.text,
+      "email": _emailController.text,
+      "gender": selectedGender,
+    });
+
+    try {
+      final response = await http.post(url, headers: headers, body: body);
+      final responseData = jsonDecode(response.body);
+
+      if (response.statusCode == 201) {
+        _showMessage("회원가입 성공: ${responseData['message']}");
+      } else {
+        _showMessage("회원가입 실패: ${responseData['message']}");
+      }
+    } catch (e) {
+      _showMessage("회원가입 중 오류 발생");
+    }
+  }
+
+  void _showMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const LogoAppBar(),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          const Center(
-            child: Text(
-              "회원가입",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Center(
+              child: Text(
+                "회원가입",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          Expanded(
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 5,
-                        spreadRadius: 2,
-                        offset: Offset(2, 2),
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      // 프로필 이미지
-                      Center(
-                        child: GestureDetector(
-                          onTap: _pickImage,
-                          child: Container(
-                            width: 80,
-                            height: 80,
-                            decoration: BoxDecoration(
-                              color: Colors.grey[400],
-                              borderRadius: BorderRadius.circular(10),
-                              image: _image != null
-                                  ? DecorationImage(
-                                      image: FileImage(_image!),
-                                      fit: BoxFit.cover,
-                                    )
-                                  : null,
-                            ),
-                            child: _image == null
-                                ? const Icon(
-                                    Icons.camera_alt,
-                                    size: 40,
-                                    color: Colors.white10,
-                                  )
-                                : null,
-                          ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 5,
+                      spreadRadius: 2,
+                      offset: Offset(2, 2),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // 프로필 이미지
+                    Center(
+                      child: GestureDetector(
+                        onTap: _pickImage,
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundColor: Colors.grey[400],
+                          backgroundImage: kIsWeb
+                              ? (_webImage != null
+                                  ? MemoryImage(_webImage!) // 웹 환경 이미지
+                                  : null)
+                              : (_image != null
+                                  ? FileImage(_image!) // 모바일 환경 이미지
+                                  : null),
+                          child: (_image == null && _webImage == null)
+                              ? const Icon(
+                                  Icons.camera_alt,
+                                  size: 40,
+                                  color: Colors.white10,
+                                )
+                              : null,
                         ),
                       ),
-                      const SizedBox(height: 10),
-                      
-                      TextField(
-                        controller: _idController,
-                        decoration: const InputDecoration(labelText: 'ID'),
-                      ),
-                      const SizedBox(height: 10),
-                      TextField(
-                        controller: _passwordController,
-                        decoration: const InputDecoration(labelText: '비밀번호'),
-                        obscureText: true,
-                      ),
-                      const SizedBox(height: 20),
+                    ),
+                    const SizedBox(height: 10),
 
-                      TextField(
-                        controller: _nicknameController,
-                        decoration: const InputDecoration(labelText: '닉네임'),
-                      ),
-                      const SizedBox(height: 10),
-                      TextField(
-                        controller: _ageController,
-                        decoration: const InputDecoration(
-                          hintText: '000000',
-                          labelText: '생년월일',
-                        ),
-                      ),
-                      const SizedBox(height: 10),
+                    TextField(
+                      controller: _idController,
+                      decoration: const InputDecoration(labelText: 'ID'),
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(labelText: '비밀번호'),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
 
-                      //비밀번호 찾기용
-                      TextField(
-                        controller: _phoneController,
-                        decoration: const InputDecoration(
-                          hintText: '010-0000-0000',
-                          labelText: '전화번호',
-                        ),
+                    TextField(
+                      controller: _nicknameController,
+                      decoration: const InputDecoration(labelText: '닉네임'),
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: _ageController,
+                      decoration: const InputDecoration(
+                        hintText: '000000',
+                        labelText: '생년월일',
                       ),
-                      const SizedBox(height: 10),
-                      TextField(
-                        controller: _emailController,
-                        decoration: const InputDecoration(labelText: 'e-mail'),
-                      ),
-                      const SizedBox(height: 20),
+                    ),
+                    const SizedBox(height: 10),
 
-                      const Text(
-                        "성별",
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                    //비밀번호 찾기용
+                    TextField(
+                      controller: _phoneController,
+                      decoration: const InputDecoration(
+                        hintText: '010-0000-0000',
+                        labelText: '전화번호',
                       ),
-                      Row(
-                        children: [
-                          Radio<String>(
-                            value: "남성",
-                            groupValue: selectedGender,
-                            onChanged: (value) {
-                              setState(() {
-                                selectedGender = value;
-                              });
-                            },
-                          ),
-                          const Text("남성"),
-                          const SizedBox(width: 20),
-                          Radio<String>(
-                            value: "여성",
-                            groupValue: selectedGender,
-                            onChanged: (value) {
-                              setState(() {
-                                selectedGender = value;
-                              });
-                            },
-                          ),
-                          const Text("여성"),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      const Text(
-                        "자기소개 (20자 미만)",
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      TextField(
-                        controller: _bioController,
-                        maxLength: 20,
-                        decoration: const InputDecoration(
-                          hintText: "자기소개를 입력하세요.",
-                          counterText: "",
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            print("ID: \${_idController.text}");
-                            print("비밀번호: \${_passwordController.text}");
-                            print("성별: \$selectedGender");
-                            print("자기소개: \${_bioController.text}");
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(labelText: 'e-mail'),
+                    ),
+                    const SizedBox(height: 20),
+
+                    const Text(
+                      "성별",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Radio<bool>(
+                          value: true,
+                          groupValue: selectedGender,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedGender = value;
+                            });
                           },
-                          child: const Text("가입하기"),
                         ),
+                        const Text("남성"),
+                        const SizedBox(width: 20),
+                        Radio<bool>(
+                          value: false,
+                          groupValue: selectedGender,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedGender = value;
+                            });
+                          },
+                        ),
+                        const Text("여성"),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      "자기소개 (20자 미만)",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    TextField(
+                      controller: _bioController,
+                      maxLength: 20,
+                      decoration: const InputDecoration(
+                        hintText: "자기소개를 입력하세요.",
+                        counterText: "",
                       ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: _registerUser,
+                        child: const Text("가입하기"),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       // 광고 영역  - 배너 클릭시 링크 연결되게 수정해야 하니 버튼으로 변경해야함
       bottomNavigationBar: const AdBanner(),
